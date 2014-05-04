21c3fc975b8a2a699042d23d22a2665ae9b68d86
Ronan Guilloux <ronan.guilloux@gmail.com>
2014-05-04 22:55:47 +0200

    use Tumblr API Client to fetch & display posts

--- src/js/main.js
+++ src/js/main.js
@@ -8,5 +8,14 @@ jQuery(document).ready(function ($) {
 
     var tumblr = new Tumblr();
     tumblr.init($);
+    tumblr.getData(function(data) {
+        if('OK' == data.meta.msg) {
+            $('section.posts').append($('<h2>').text("Tumblr :"));
+            for(var i in data.response.posts) {
+                $('section.posts').append($('<article>').html(data.response.posts[i].body));
+            }
+        }
+    });
+
 
 });
--- src/js/vendor/Tumblr.js
+++ src/js/vendor/Tumblr.js
@@ -9,7 +9,7 @@ var Tumblr = function(){
     var params = {
             key:    'JF1gaswtw9E1npDP7mtDhBfhgsdxRrhovUBIF52VPJj13hnzFZ',
             base_url:  'http://api.tumblr.com/v2/blog/euradionantes-ee2014.tumblr.com/',
-            default_type: 'posts',
+            default_type: 'posts/text',
         };
     var jQuery;
 
@@ -19,21 +19,20 @@ var Tumblr = function(){
      */
     var init = function($) {
         jQuery = $;
-        return getData();
     }
 
     var getUrl = function () {
         return params.base_url + params.default_type + '?api_key=' + params.key;
     }
 
-    var getData = function() {
-        var api = getUrl();
-        jQuery.getJSON( api, {})
-            .done(function( data ) {
-                jQuery.each( data.items, function( key, item ) {
-                    console.log(key,data);
-                });
-            });
+    var getData = function(callBack) {
+        jQuery.ajax({
+            url: getUrl(),
+            dataType: 'jsonp',
+            success: function(data){
+                return callBack(data);
+            }
+        });
     }
 
     /**
