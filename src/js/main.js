a859ecd8dd9cd211075279481ba9dda665bc78ff
Ronan Guilloux <ronan.guilloux@gmail.com>
2014-05-07 18:59:10 +0200

    no more anayzer for now

--- src/js/main.js
+++ src/js/main.js
@@ -12,8 +12,10 @@ var video = {
 
 jQuery('document').ready(function() {
 
+    /*
     var analyzer = new GoogleAnalyzer();
     analyzer.init();
+    */
 
     var tumblr = new Tumblr(),
     $poster = $('#cold-tmblr .container');
