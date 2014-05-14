7f99dc3832a42ce30a34746916c6ed8343b1d149
Ronan Guilloux <ronan.guilloux@gmail.com>
2014-05-14 10:01:19 +0200

    concerns #2297: multiple RSS parsing

--- src/js/main.js
+++ src/js/main.js
@@ -12,54 +12,68 @@ var video = {
 
 jQuery('document').ready(function() {
 
-    //var analyzer = new GoogleAnalyzer();
-    //analyzer.init();
+    var $posts = $('#cold-tmblr'),
+        $feeds = $('#feeds');
 
-    var tumblr = new Tumblr(),
-    $poster = $('#cold-tmblr .container');
+    //--- Posts ----------------------------------------------
+    if(0 < $posts.length) {
+        var tumblr = new Tumblr();
+        tumblr.init($);
+        tumblr.getPosts(function(data) {
+            if('OK' == data.meta.msg) {
+                $posts = $('#cold-tmblr');
+                for(var i in data.response.posts) {
+                    $posts.append($('<article data-index="'+i+'">').html(
+                        '<div class="togglable summary" style="display:block">'
+                        + '<h1><a href="#">' + data.response.posts[i].title + '</a></h1>'
+                        + data.response.posts[i].summary
+                        + '</div><div class="togglable body" style="display:none">'
+                        + '<h1><a href="#">' + data.response.posts[i].title + '</a></h1>'
+                        + data.response.posts[i].body
+                        + '</div>'
+                        )
+                    );
+                }
 
-    tumblr.init($);
-
-    tumblr.getPosts(function(data) {
-        if('OK' == data.meta.msg) {
-            $poster = $('#cold-tmblr');
-            for(var i in data.response.posts) {
-                $poster.append($('<article data-index="'+i+'">').html(
-                    '<div class="togglable summary" style="display:block">'
-                    + '<h1><a href="#">' + data.response.posts[i].title + '</a></h1>'
-                    + data.response.posts[i].summary
-                    + '</div><div class="togglable body" style="display:none">'
-                    + '<h1><a href="#">' + data.response.posts[i].title + '</a></h1>'
-                    + data.response.posts[i].body
-                    + '</div>'
-                    )
-                );
+                $('article .togglable h1 a').on('click', function(e){
+                    e.preventDefault();
+                    $('.togglable', $(this).parent().parent().parent()).toggle();
+                });
             }
-
-            $('article .togglable h1 a').on('click', function(e){
-                e.preventDefault();
-                $('.togglable', $(this).parent().parent().parent()).toggle();
-            });
-        }
-    });
-
-    tumblr.getVideos(function(data) {
-        if('OK' == data.meta.msg) {
-            /*
-            if(0 < data.response.posts.length) {
-                $('#live-media-video .video-container').empty().append($('<div>', {
-                    id: video.iframeId,
-                }));
-                video.videoId = data.response.posts[0].youtube.videoId;
-                video.provider = new Youtube();
-                video.provider.init(video.videoId, video.iframeId, video.vars, false);
+        });
+        tumblr.getVideos(function(data) {
+            if('OK' == data.meta.msg) {
+                /*
+                 if(0 < data.response.posts.length) {
+                     $('#live-media-video .video-container').empty().append($('<div>', {
+                         id: video.iframeId,
+                     }));
+                     video.videoId = data.response.posts[0].youtube.videoId;
+                     video.provider = new Youtube();
+                     video.provider.init(video.videoId, video.iframeId, video.vars, false);
+                 }
+                 */
             }
-            */
-        }
-    });
+        });
+    }
+
+    //--- Feeds ----------------------------------------------
+    if(0 < $feeds.length) {
+
+        var rss = new RssParser();
+        rss.init($);
+        rss.parse(function(data) {
+            if(0 < data.value.items.length) {
+                for(var i in data.value.items) {
+                    console.log('iteraring:',data[i]);
+                }
+            }
+        });
+    }
 
     // $('#live-media-video .video-container').empty().html('<iframe src="http://www.glowbl.com/EGE" frameborder="0" style="width:100% height:100%"/>');
 
+    //--- Tabs ----------------------------------------------
     // Tab click event handlers + consequences on video/audio players
     $('#tab-video').on('click', function(e){
         e.preventDefault();
